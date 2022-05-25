fc8f78d12b18481f1a04cf346b8120fb8ba612eb
SWWI <swwirin@gmail.com>
2022-05-25 20:51:28 +0500

    refresh1

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-def index(request):
-    return HttpResponse('<h1> Hello world</h1>')
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+# from django.http import HttpResponse
+# # Create your views here.
+from django.shortcuts import render
+from .models import Question
+# ef index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'swwi': latest_question_list,
+#         'hello': 'World'
+#     }
+#     return HttpResponse(template.render(context, request))
+last  = Question.object.first()
+context = {
+    'obj': last
+}
+return render(request "polls/index.html", context)
